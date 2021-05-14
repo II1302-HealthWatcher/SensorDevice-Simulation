dbdfa2f05b4005c842dab102d28b549c8e9c256f
Alexander Lundqvist <33635814+KrimReaper@users.noreply.github.com>
2021-05-14 22:57:49 +0200

    Fixed final styling, added some functionality

--- src/main/java/se/kth/ii1302/healthwatcher/view/MainView.java
+++ src/main/java/se/kth/ii1302/healthwatcher/view/MainView.java
@@ -24,6 +24,7 @@ public class MainView {
     private Date testDate;
     private int amount;
     private String[] responses;
+    private int delay;
     
     /**
      * Constructs a MainView and sets the controller field.
@@ -34,9 +35,10 @@ public class MainView {
         this.output = new JTextArea("");
         this.output.setEditable(false);
         this.measurements = null;
-        this.testDate = new Date(121, 4, 1, 7, 30);
-        this.amount = 10;
+        this.testDate = null;
+        this.amount = 0;
         this.responses = null;
+        this.delay = 0;
     }
     
     /**
@@ -44,7 +46,6 @@ public class MainView {
      * different operations from a menu and results will be printed in the GUI.
      */
     public void runInterface() {
-
         try {
             // URL that helps with sending data tests "https://httpbin.org/anything"
             // URL for the server management server "http://5.150.209.79:1234/"
@@ -52,7 +53,8 @@ public class MainView {
             // Creating the frame
             JFrame frame = new JFrame("HealthWatcher Sensor Simulation");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(800, 500);
+            frame.setSize(900, 500);
+            frame.setLocationRelativeTo(null);
 
             // Adding filler at the top
             JPanel filler = new JPanel();
@@ -60,18 +62,24 @@ public class MainView {
 
             // Static text area with option menu
             JTextArea textArea = new JTextArea(
-                 "Available actions \n\n"+
-                 "1. Get device id. \n"+
-                 "2. Set certain device id. \n"+
-                 "3. Generate a random device id. \n"+
-                 "4. Generate measurement for the current device. \n"+
-                 "5. Generate multiple measurements for the current device. \n"+
-                 "6. Generate critical measurement for the current device. \n"+     
-                 "7. Get the current generated measurements. \n"+
-                 "8. Send the current measurements to the server. \n"+
-                 "9. Print server responses. \n"
+                 "  Available actions \n\n"+
+                 "  1. Get device id. \n"+
+                 "  2. Set certain device id. \n"+
+                 "  3. Generate a random device id. \n"+
+                 "  4. Generate measurement for the current device. \n"+
+                 "  5. Generate multiple measurements for the current device. \n"+
+                 "  6. Generate critical measurement for the current device. \n"+     
+                 "  7. Get the current generated measurements. \n"+
+                 "  8. Send the current measurements to the server. \n"+
+                 "  9. Print server responses. \n"
              );
+            // Setting the properties of the textarea
             textArea.setEditable(false);
+            textArea.setPreferredSize(new Dimension(400, 500));
+            Color defaultColor = UIManager.getColor("Panel.background");
+            textArea.setBackground(defaultColor);
+            Font font = textArea.getFont();  
+            textArea.setFont(font.deriveFont(Font.BOLD));
             frame.getContentPane().add(BorderLayout.LINE_START, textArea);
 
             // Scrollable area for function output  
@@ -103,7 +111,6 @@ public class MainView {
                 } catch (IOException ex) {
                     Logger.getLogger(MainView.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                
             });
 
             // Generate random id
@@ -121,7 +128,8 @@ public class MainView {
             // Generate measurement
             JButton button4 = new JButton("4");
             button4.addActionListener((ActionEvent e) -> {
-                this.controller.generateMeasurementForDevice(testDate);
+                Date newDate = convertToDate();
+                this.controller.generateMeasurementForDevice(newDate);
                 measurements = this.controller.getGeneratedMeasurements();
                 output.append("- A measurement has been generated." + "\n");
             });
@@ -129,7 +137,9 @@ public class MainView {
             // Generate multiple measurements
             JButton button5 = new JButton("5");
             button5.addActionListener((ActionEvent e) -> {
-                this.controller.generateMeasurementsForDevice(testDate, amount);
+                Date newDate = convertToDate();
+                String inputAmount = JOptionPane.showInputDialog(frame,"Please write how many measurements you want.");
+                this.controller.generateMeasurementsForDevice(newDate, Integer.parseInt(inputAmount));
                 measurements = this.controller.getGeneratedMeasurements();
                 output.append("- Measurements has been generated." + "\n");
             });
@@ -137,7 +147,8 @@ public class MainView {
             // Generate critical measurement
             JButton button6 = new JButton("6");
             button6.addActionListener((ActionEvent e) -> {
-                this.controller.generateCriticalMeasurementForDevice(testDate);
+                Date newDate = convertToDate();
+                this.controller.generateCriticalMeasurementForDevice(newDate);
                 measurements = this.controller.getGeneratedMeasurements();
                 output.append("- Critical measurement has been generated." + "\n");
             });
@@ -148,26 +159,28 @@ public class MainView {
                 resetOutput();
                 output.append("- These are your current measurements: \n");
                 showMeasurements(measurements);
-                
             });
             
             // Send measurements to server
             JButton button8 = new JButton("8");
             button8.addActionListener((ActionEvent e) -> {
+                String inputDelay = JOptionPane.showInputDialog(frame,"Please write the desired delay in seconds.");
                 try {
-                    responses = this.controller.sendMeasurements(2);
+                    responses = this.controller.sendMeasurements(Integer.parseInt(inputDelay));
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
                 output.append("- Measurements sent to server." + "\n");
             });
             
+            // Show the server responses
             JButton button9 = new JButton("9");
             button9.addActionListener((ActionEvent e) -> {
                 resetOutput();
                 showResponses(responses);
             });
             
+            // Reset the output console
             JButton button10 = new JButton("Clear");
             button10.addActionListener((ActionEvent e) -> {
                 resetOutput();
@@ -210,6 +223,35 @@ public class MainView {
             output.append("Response nr: " + (i +1) + "\n");
             output.append(responses[i].replaceAll("\n", "") + "\n\n");
         }
+    }
+    
+    // Helper function to assist in parsing strings to date
+    private Date convertToDate(){
+        Date parsedDate = null;
+        JTextField yearField = new JTextField();
+        JTextField monthField = new JTextField();
+        JTextField dayField = new JTextField();
+        JTextField hourField = new JTextField();
+        JTextField minuteField = new JTextField();
+        Object[] message = {
+            "Year", yearField,
+            "Month", monthField,
+            "Day", dayField,
+            "Hour", hourField,
+            "Minute", minuteField,
+        };
+        int option = JOptionPane.showConfirmDialog(null, message, "Enter all your values", JOptionPane.OK_CANCEL_OPTION);
+        if (option == JOptionPane.OK_OPTION)
+        {
+            String year = yearField.getText();
+            String month = monthField.getText();
+            String day = dayField.getText();
+            String hour = hourField.getText();
+            String minute = minuteField.getText();
+            parsedDate = new Date(Integer.parseInt(year)-1900,Integer.parseInt(month)-1,Integer.parseInt(day),Integer.parseInt(hour),Integer.parseInt(minute));
+        }
+        
+        return parsedDate;
     }
     
     private void resetOutput(){
